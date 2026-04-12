233c34aae03e662a88b0cf5e0d1f00e0249a543d
HungPhamNoob <hungvp711@gmail.com>
2026-04-13 04:03:01 +0700

    feat: add zalopay/vnpay

--- PeakGear-hung1 (1)/PeakGear-hung1/src/app/code/Utklasad/AdminProductGridCategoryFilter/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Utklasad_AdminProductGridCategoryFilter',
-    __DIR__
-);
--- PeakGear-hung1 (1)/PeakGear-hung1/src/app/code/Vendor/CurrencyRate/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Vendor_CurrencyRate',
-    __DIR__
-);
